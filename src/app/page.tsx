a7dff852ec6276be69b3c024b7fc99b685283ac4
Alex Zalo <zalo.alex56@gmail.com>
2024-11-25 19:27:35 +0100

    Update Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,44 +5,28 @@ export default function Home() {
         <>
             <div className="content-block header">
                 <h1>Universal Decentralized Encryption System <br />- UDES - </h1>
-                <span>UDES offers a solution to secure your communications directly from your web browser, ensuring confidentiality, integrity, and authenticity in your exchanges. Protect your instant messages with our end-to-end encryption. Download now for maximum security.</span>
+                <span>UDES offers a solution to secure your communications directly from your web browser, ensuring confidentiality, integrity, and authenticity in your exchanges. Download now for maximum security.</span>
                 <div>
                     <a className="button green" href="/extensions">Download</a>
                 </div>
             </div>
             <div className="content-block">
                 <h2>How it Works</h2>
-                <span><p><strong>UDES</strong> makes your messages more secure and works in two ways:</p><br />
+                <div className="hiw-content">
+                    <p><strong>UDES</strong> makes your messages more secure and works in two ways:</p><br />
 
-<p><strong>1. Using its own chat:</strong><br />
-Access a secure chat provided by UDES to communicate with full confidentiality.</p><br />
+                    <p><strong>1. Using our chat to communicate with full confidentiality.</strong></p>
+                    <p><strong>2. With others supported applications:</strong></p><br />
 
-<p><strong>2. With applications like WhatsApp Web or Discord:</strong><br />
-This requires Firefox and the installation of the UDES extension to ensure message protection.</p><br />
+                    <p>To ensure security, both you and your contact need to install UDES. Here are the steps:</p><br />
 
-<hr />
-
-<p>To ensure security, both you and your contact need to install UDES. Here are the steps:</p><br />
-
-<ul>
-    <li><strong>Install the UDES extension:</strong><br />
-        Download and install the UDES extension on Firefox for both you and your contact.</li><br />
-
-    <li><strong>Connection and extension activation:</strong><br />
-        Log in to your usual messaging app with the UDES extension activated on Firefox.</li><br />
-
-    <li><strong>Automatic protection:</strong><br />
-        Once connected, UDES encrypts your messages in the background. No further action is required.</li><br />
-
-    <li><strong>Message visibility:</strong><br />
-        Messages are encrypted and visible only to those who have installed UDES. Without the extension, even if someone accesses the conversation, the messages will remain unreadable.</li>
-</ul><br />
-
-<hr />
-
-<p><strong>In summary:</strong><br />
-UDES keeps your conversations private. As long as both you and your contact use UDES, your exchanges are automatically secured and invisible to others.</p>
-</span>
+                    <ul>
+                        <li><a href="/extensions">Install</a> the UDES extension.</li>
+                        <li>Log in to your usual messaging app with the UDES extension activated.</li>
+                        <li>Once connected, The UDES extension encrypts your messages in the background. No further action is required.</li>
+                        <li>Messages are encrypted and visible only to those who have installed UDES. Without the extension, even if someone accesses the conversation, the messages will remain unreadable.</li>
+                    </ul><br />
+                </div>
             </div>
             <div className="content-block specification">
                 <div className="block">
@@ -58,7 +42,7 @@ UDES keeps your conversations private. As long as both you and your contact use 
                 <div className="block">
                     <img src="/static/assets/icons/community.png" alt="" />
                     <h3>Community</h3>
-                    <span>Join a community committed to secure and private exchanges. UDES connects users who share the same values, creating a trusted and secure network.</span>
+                    <span>UDES lives thanks to the work of the community in keeping the software up to date. Join us to help users have a better experience.</span>
                 </div>
             </div>
         </>
