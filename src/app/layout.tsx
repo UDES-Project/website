8cab8246725b9ca6cf971c71cdc1d774b94cb269
Alex Zalo <zalo.alex56@gmail.com>
2024-12-05 19:35:24 +0100

    Title + Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import NavBar from "@/components/navbar/navbar";
 import "./globals.scss";
 import Footer from "@/components/footer/footer";
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'UDES - Universal Decentralized Encryption System',
+    description: 'UDES can be used to encrypt anything using decentralised encryption servers. Its primary purpose is to encrypt messages on the client side to increase privacy.',
+}
 
 export default function RootLayout({
     children,
@@ -10,11 +16,11 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <NavBar/>
+                <NavBar />
                 <main>
                     {children}
                 </main>
-                <Footer/>
+                <Footer />
             </body>
         </html>
     );
